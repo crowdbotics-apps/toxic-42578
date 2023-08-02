26c02a322ceb28fc9733e57fb3c8674d0130c915
Crowdbotics <team@crowdbotics.com>
2023-08-02 01:14:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "toxic-42578",
+  url: "https://toxic-42578.botics.co",
+  api: "https://toxic-42578.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
